2cfac7ee4452e6c3bc0d90e1b8c104d36fa406ca
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-03-01 20:04:16 +0900

    start making Weather App

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello! I made a RN App! 장준수 화이팅ㅋㅋ</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "tomato" }}>
+      <StatusBar></StatusBar>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
